a972ea94a0e731adb72ad4bb0391c2255a287e73
martin reda <martinreda999@outlook.com>
2025-07-22 17:13:35 +0300

    01 Class

--- Demo03OOP/Program.cs
+++ Demo03OOP/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace Demo03OOP
+﻿using System.Reflection;
+
+namespace Demo03OOP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region User Defiene Datatype [Class] 
+            /*
+             * Blueprint contain Objects [member variables , methods ]
+             * class is referance type object stored in stack and values stored in heap 
+             * ComPiler Will Generate Empty Parameters CTOR
+             * if user defiend CTOR found , Compiler Willno longer need parameterless CTOR
+             *   - Attributes 
+             *   - Functions 
+             *   - Methods
+             *   - EVent
+             * 6 Access modifier Allowed 
+             * Defual access mod inside class Privte 
+             * Defualt Internal 
+             * */
+
+            #endregion
+            Car C1; 
+            //declars referance type from 'Car' Class 
+            //C1 refer to defualt in heap 
+            // this referance C1 can refer to an instance of type Car or any Data type inhiert from Car
+            // //CLR will allocatr 4 byte at stack for object C1 
+
+            C1 = new Car(10 , "BMW", 600 ); // hna lazm values  () 3l4an al CROT ---
+                                                // lw 3aiaz ben al () kda lazm a3ml Default cTOR
+                                                // id = 0; model = "Unknown"; 
+
+            Console.WriteLine( C1);
+
+            Car C2 = new Car(20, "fait");
+            Console.WriteLine(C2);
+
+            Car c3 = new Car(10);
+            Console.WriteLine(c3);
         }
     }
 }
