102c0dae1a35436461d6a01ebf7a22e2945294e5
martin reda <martinreda999@outlook.com>
2025-07-23 15:20:02 +0300

    Q 02

--- Assinggmnet03OOP/Program.cs
+++ Assinggmnet03OOP/Program.cs
@@ -7,12 +7,12 @@
             #region Part01
             Employee emp = new Employee(1, "Martin Reda", SecurityLevel.Developer, 12000.5, new DateTime(2023, 3, 15), Gender.M);
             Console.WriteLine(emp);
-            #endregion
+            #endregion 
             #region
             Console.WriteLine( "hello world ");
             #endregion
 
-            //mfmf
+            
         }
     }
 }
