4fe621374cc9fab231375e18976faaa226d72587
martin reda <martinreda999@outlook.com>
2025-07-23 15:06:48 +0300

    Part01#

---
+++ Assinggmnet03OOP/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Assinggmnet03OOP
+{
+    internal class Employee
+    {
+        #region Properties
+        public int Id { get; set; }
+        public string? name { get; set; }
+        public SecurityLevel SecurityLevel { get; set; }
+        public double Salary { get; set; }
+        public DateTime HireDate { get; set; }
+        public Gender Gender { get; set; }
+
+        #endregion
+
+
+
+        #region constractor
+        public Employee(int _Id, string _Name, SecurityLevel _securityLevel, double _salary, DateTime _hireDate, Gender _gender)
+        {
+            Id = _Id;
+            name = _Name;
+            SecurityLevel = _securityLevel;
+            Salary = _salary;
+            HireDate = _hireDate;
+            Gender = _gender;
+        }
+        #endregion
+        #region Method
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {name}, Gender: {Gender}, Security Level: {SecurityLevel}, " +
+                   $"Salary: {string.Format("{0:C}", Salary)}, Hire Date: {HireDate.ToShortDateString()}";
+        }
+        #endregion
+    }
+}
--- Assinggmnet03OOP/Program.cs
+++ Assinggmnet03OOP/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part01
+            Employee emp = new Employee(1, "Martin Reda", SecurityLevel.Developer, 12000.5, new DateTime(2023, 3, 15), Gender.M);
+            Console.WriteLine(emp);
+            #endregion
+            #region
+            Console.WriteLine( "hello world ");
+            #endregion
+
+            //mfmf
         }
     }
 }
